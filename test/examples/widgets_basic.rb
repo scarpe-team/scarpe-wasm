b2749d86132516c67779ee34c9d84e1ef3b29510
Noah Gibbs <the.codefolio.guy@gmail.com>
2023-08-24 18:55:07 +0100

    Add more widget types to make sure they don't immediately break

--- test/examples/widgets_basic.rb
+++ test/examples/widgets_basic.rb
@@ -6,6 +6,19 @@ Shoes.app do
     button "Push me"
     alert "I am an alert!"
     edit_line "edit_line here", width: 450
+
+    # Image has a conflict in Lacci because of image size
     #image "http://shoesrb.com/manual/static/shoes-icon.png"
+  end
+  flow do
+    arc(400, 0, 120, 100, 175, 175)
+    check
+    edit_box("bar")
+    line(0, 0, 100, 100)
+    list_box(items: ['A', 'B'])
+    radio("gaga")
+    shape { line(0, 0, 10, 10) }
+    star(230, 100, 6, 50, 25)
+    video("http://techslides.com/demos/sample-videos/small.mp4")
   end
 end
